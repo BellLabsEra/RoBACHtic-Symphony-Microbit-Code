d088e7e4fa803bceb423e573453a3b9730eda852
Christopher Semali <94195261+BellLabsEra@users.noreply.github.com>
2023-11-07 00:44:33 -0500

    added all the resetServoPositions() and moveArmToString(new_shoulder_angle, new_elbow_angle) methods to the ECE3011_Soft_Demo codebase

--- main.ts
+++ main.ts
@@ -34,6 +34,80 @@ function readPianoKeyboardInput () {
     keyboard_input_decimal_representation = MSB * 8 + LSB_2 * 4 + LSB_1 * 2 + LSB_0 * 1
     return keyboard_input_decimal_representation
 }
+function doSomething () {
+    moveArmToString(G_STRING_SHOULDER_JOINT_ANGLE, G_STRING_ELBOW_JOINT_ANGLE)
+    basic.pause(2000)
+    moveArmToString(D_STRING_SHOULDER_JOINT_ANGLE, D_STRING_ELBOW_JOINT_ANGLE)
+    basic.pause(2000)
+    moveArmToString(A_STRING_SHOULDER_JOINT_ANGLE, A_STRING_ELBOW_JOINT_ANGLE)
+    basic.pause(2000)
+    moveArmToString(E_STRING_SHOULDER_JOINT_ANGLE, E_STRING_ELBOW_JOINT_ANGLE)
+    basic.pause(2000)
+}
+function moveArmToString (new_shoulder_angle: number, new_elbow_angle: number) {
+    if (shoulder_joint_angle < new_shoulder_angle) {
+        while (shoulder_joint_angle < new_shoulder_angle) {
+            servos.P0.setAngle(shoulder_joint_angle)
+            basic.pause(100)
+            shoulder_joint_angle += 1
+        }
+    } else {
+        while (shoulder_joint_angle > new_shoulder_angle) {
+            servos.P0.setAngle(shoulder_joint_angle)
+            basic.pause(100)
+            shoulder_joint_angle += -1
+        }
+    }
+    if (elbow_joint_angle < new_elbow_angle) {
+        while (elbow_joint_angle < new_elbow_angle) {
+            servos.P1.setAngle(elbow_joint_angle)
+            basic.pause(100)
+            elbow_joint_angle += 1
+        }
+    } else {
+        while (elbow_joint_angle > new_elbow_angle) {
+            servos.P1.setAngle(elbow_joint_angle)
+            basic.pause(100)
+            elbow_joint_angle += -1
+        }
+    }
+}
+function resetServoPositions () {
+    wrist_joint_angle = 45
+    elbow_joint_angle = 45
+    shoulder_joint_angle = 45
+    LEGATO_UP_BOW_WRIST_JOINT_ANGLE = 0
+    MID_BOW_WRIST_JOINT_ANGLE = 90
+    DOWN_BOW_WRIST_JOINT_ANGLE = 180
+    E_STRING_ELBOW_JOINT_ANGLE = 35
+    A_STRING_ELBOW_JOINT_ANGLE = 35
+    D_STRING_ELBOW_JOINT_ANGLE = 35
+    G_STRING_ELBOW_JOINT_ANGLE = 25
+    E_STRING_SHOULDER_JOINT_ANGLE = 180
+    A_STRING_SHOULDER_JOINT_ANGLE = 145
+    D_STRING_SHOULDER_JOINT_ANGLE = 90
+    G_STRING_SHOULDER_JOINT_ANGLE = 0
+    servos.P0.setPulse(20000)
+    servos.P1.setPulse(20000)
+    servos.P2.setPulse(20000)
+    servos.P0.setAngle(shoulder_joint_angle)
+    servos.P1.setAngle(elbow_joint_angle)
+    servos.P2.setAngle(wrist_joint_angle)
+}
+let DOWN_BOW_WRIST_JOINT_ANGLE = 0
+let MID_BOW_WRIST_JOINT_ANGLE = 0
+let LEGATO_UP_BOW_WRIST_JOINT_ANGLE = 0
+let wrist_joint_angle = 0
+let elbow_joint_angle = 0
+let shoulder_joint_angle = 0
+let E_STRING_ELBOW_JOINT_ANGLE = 0
+let E_STRING_SHOULDER_JOINT_ANGLE = 0
+let A_STRING_ELBOW_JOINT_ANGLE = 0
+let A_STRING_SHOULDER_JOINT_ANGLE = 0
+let D_STRING_ELBOW_JOINT_ANGLE = 0
+let D_STRING_SHOULDER_JOINT_ANGLE = 0
+let G_STRING_ELBOW_JOINT_ANGLE = 0
+let G_STRING_SHOULDER_JOINT_ANGLE = 0
 let keyboard_input_decimal_representation = 0
 let LSB_0 = 0
 let LSB_1 = 0
@@ -41,6 +115,7 @@ let LSB_2 = 0
 let midi_notes_octave1_dict: number[] = []
 let notes_dictionary: string[] = []
 let MSB = 0
+resetServoPositions()
 let current_state = 0
 let next_state = 0
 let staccato_legato_state = 0
@@ -103,4 +178,5 @@ midi_notes_octave1_dict = [
 ]
 basic.forever(function () {
     stateTransitions(0, readPianoKeyboardInput())
+    doSomething()
 })
